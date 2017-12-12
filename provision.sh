2d5790b913ac085f2f757001cfba23757c935637
joravlo <joravlo@gmail.com>
2017-12-12 11:48:07 +0100

    Cambios Actividad 3B

--- provision.sh
+++ provision.sh
@@ -1,2 +1,7 @@
 apt-get -y update
 apt-get install -y apache2
+sudo apt-get -y install git
+sudo rm -rf /var/www/html
+cd /vagrant
+git clone https://github.com/boputu/adventuresport.git
+sudo ln -fs /vagrant/adventuresport /var/www/html
